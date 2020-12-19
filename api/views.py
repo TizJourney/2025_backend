07229c552a4084779c51886da69426107c70729b
Konstantin Bondar <kostya.bondar@gmail.com>
2020-12-20 02:54:01 +0300

    feat: новая версия views

--- api/views.py
+++ api/views.py
@@ -1,67 +1,58 @@
 from rest_framework import (decorators, response, serializers, status)
-from .serializers import VerifyOutputSerializer, VerifyInputSerializer
+from .serializers import VerifyOutputSerializer, VerifyInputSerializer, CitateSerializer
 from rest_framework.decorators import api_view
 from pymystem3 import Mystem
 import re
-import pandas as pd
-import numpy as np
 from sklearn.feature_extraction.text import TfidfVectorizer
 import nltk
 from sklearn.metrics.pairwise import cosine_similarity
 
-from .models import Poem, Citate
+from .models import Citate
 
 
-# nltk.download('stopwords')
+nltk.download('stopwords')
 
+class QueryConverter:
+    def __init__(self):
+        self.stemmer = Mystem()
+        self.stop_words = set(nltk.corpus.stopwords.words('russian'))
 
-# class QueryConverter:
-#     def __init__(self):
-#         self.stemmer = Mystem()
-#         self.citate_corpus = pd.read_csv('./data/text_corpus.csv')
-#         self.citate_corpus.dropna(inplace=True)
+    def lemmatize(self, text):
+        lemm_list = self.stemmer.lemmatize(text)
+        lemm_text = ''.join(lemm_list).strip('\n')
 
-#         stop_words = set(nltk.corpus.stopwords.words('russian'))
+        return lemm_text
 
-#         self.tf_idf = TfidfVectorizer(stop_words=stop_words)
+    def clean(self, text):
+        return ' '.join(re.sub(r'[^а-яё ]', ' ', text.lower()).split())
 
-#         self.tf_idf_data = self.tf_idf.fit_transform(
-#             self.citate_corpus['lemmed_line']
-#         )
+    def query_transform(self, text):
+        return self.lemmatize(self.clean(text))
 
-#     def lemmatize(self, text):
-#         lemm_list = self.stemmer.lemmatize(text)
-#         lemm_text = ''.join(lemm_list).strip('\n')
+    def find_similar(self, text, number=10):
+        lemmed_text = self.query_transform(text)
 
-#         return lemm_text
+        start_of_text = ' '.join(lemmed_text.split(' ')[:3])
 
-#     def clean(self, text):
-#         return ' '.join(re.sub(r'[^а-яё ]', ' ', text.lower()).split())
+        candidate_objects = Citate.objects.prefetch_related('poem').filter(lemmed_line__icontains=start_of_text)
+        self.tf_idf = TfidfVectorizer(stop_words=self.stop_words)
+        lemmed_class_lines = [item.lemmed_line for item in candidate_objects]
 
-#     def query_transform(self, text):
-#         lemmed_text = self.lemmatize(self.clean(text))
-#         print(lemmed_text)
-#         vectorized_text = self.tf_idf.transform([lemmed_text])
-#         return vectorized_text
+        self.tf_idf_data = self.tf_idf.fit_transform(lemmed_class_lines)
 
-#     def find_similar(self, text, number=10):
-#         vectorized_text = self.query_transform(text)
+        vectorized_text = self.tf_idf.transform([lemmed_text])
 
-#         cosine_similarities = cosine_similarity(
-#             vectorized_text,  self.tf_idf_data).flatten()
-#         related_product_indices = cosine_similarities.argsort()[:-number:-1]
-#         return [
-#             {
-#                 'citate': self.citate_corpus.iloc[index]['line'],
-#                 'author': self.citate_corpus.iloc[index]['author'],
-#                 'title': self.citate_corpus.iloc[index]['name'],
-#                 'score':  int(cosine_similarities[index]*100)
-#             }
-#             for index in related_product_indices
-#         ]
+        cosine_similarities = cosine_similarity(
+            vectorized_text,  self.tf_idf_data).flatten()
+        related_product_indices = cosine_similarities.argsort()[:-number:-1]
 
+        results = []
+        for index in related_product_indices:
+            slow_index = int(index)
+            item = CitateSerializer(instance=candidate_objects[slow_index], context={'score' : cosine_similarities[index]*100})
 
-# convert_instance = QueryConverter()
+            results.append(item.data)
+        return results
 
 
 @api_view(('GET',))
@@ -72,14 +63,14 @@ def verify(request):
 
     query = input_data.validated_data['query']
     citate_number = input_data.validated_data['number']
-    similar_lines = []
-    # convert_instance.find_similar(query, citate_number)
+    
+    convert_instance = QueryConverter()
+    similar_lines = convert_instance.find_similar(query, citate_number)
 
-    best_score = max(item['score'] for item in similar_lines) or 0
-    output_data = VerifyOutputSerializer(data={
+    best_score = max(item['score'] for item in similar_lines) if similar_lines else 0
+    output_data = {
         'query': query,
         'similar': similar_lines,
         'score': best_score,
-    })
-    output_data.is_valid(raise_exception=True)
-    return response.Response(output_data.data, status=status.HTTP_200_OK)
+    }
+    return response.Response(output_data, status=status.HTTP_200_OK)
