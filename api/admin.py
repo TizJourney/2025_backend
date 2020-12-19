e0cfb8852ce5f8f94c5dcd0ae718cc709b50a73e
Konstantin Bondar <kostya.bondar@gmail.com>
2020-12-19 23:10:25 +0300

    feat: обновить админку для произведений

--- api/admin.py
+++ api/admin.py
@@ -5,7 +5,7 @@ from .models import Poem
 class PoemAdmin(admin.ModelAdmin):
     list_display = ('name', 'author', 'date_to', 'date_from', 'text')
     search_fields = ('name', 'author', 'text',)
-    list_filter = ('name', 'author', 'date_to', 'date_from')
+    list_filter = ('author',)
 
 admin.site.register(Poem, PoemAdmin)
 
