d3635ac52230a7898f795b5a2ef987ed36d5fb94
Konstantin Bondar <kostya.bondar@gmail.com>
2020-12-19 23:59:54 +0300

    feat: временно отключени поиска похожих

--- api/views.py
+++ api/views.py
@@ -12,56 +12,56 @@ from sklearn.metrics.pairwise import cosine_similarity
 from .models import Poem, Citate
 
 
-nltk.download('stopwords')
+# nltk.download('stopwords')
 
 
-class QueryConverter:
-    def __init__(self):
-        self.stemmer = Mystem()
-        self.citate_corpus = pd.read_csv('./data/text_corpus.csv')
-        self.citate_corpus.dropna(inplace=True)
+# class QueryConverter:
+#     def __init__(self):
+#         self.stemmer = Mystem()
+#         self.citate_corpus = pd.read_csv('./data/text_corpus.csv')
+#         self.citate_corpus.dropna(inplace=True)
 
-        stop_words = set(nltk.corpus.stopwords.words('russian'))
+#         stop_words = set(nltk.corpus.stopwords.words('russian'))
 
-        self.tf_idf = TfidfVectorizer(stop_words=stop_words)
+#         self.tf_idf = TfidfVectorizer(stop_words=stop_words)
 
-        self.tf_idf_data = self.tf_idf.fit_transform(
-            self.citate_corpus['lemmed_line']
-        )
+#         self.tf_idf_data = self.tf_idf.fit_transform(
+#             self.citate_corpus['lemmed_line']
+#         )
 
-    def lemmatize(self, text):
-        lemm_list = self.stemmer.lemmatize(text)
-        lemm_text = ''.join(lemm_list).strip('\n')
+#     def lemmatize(self, text):
+#         lemm_list = self.stemmer.lemmatize(text)
+#         lemm_text = ''.join(lemm_list).strip('\n')
 
-        return lemm_text
+#         return lemm_text
 
-    def clean(self, text):
-        return ' '.join(re.sub(r'[^а-яё ]', ' ', text.lower()).split())
+#     def clean(self, text):
+#         return ' '.join(re.sub(r'[^а-яё ]', ' ', text.lower()).split())
 
-    def query_transform(self, text):
-        lemmed_text = self.lemmatize(self.clean(text))
-        print(lemmed_text)
-        vectorized_text = self.tf_idf.transform([lemmed_text])
-        return vectorized_text
+#     def query_transform(self, text):
+#         lemmed_text = self.lemmatize(self.clean(text))
+#         print(lemmed_text)
+#         vectorized_text = self.tf_idf.transform([lemmed_text])
+#         return vectorized_text
 
-    def find_similar(self, text, number=10):
-        vectorized_text = self.query_transform(text)
+#     def find_similar(self, text, number=10):
+#         vectorized_text = self.query_transform(text)
 
-        cosine_similarities = cosine_similarity(
-            vectorized_text,  self.tf_idf_data).flatten()
-        related_product_indices = cosine_similarities.argsort()[:-number:-1]
-        return [
-            {
-                'citate': self.citate_corpus.iloc[index]['line'],
-                'author': self.citate_corpus.iloc[index]['author'],
-                'title': self.citate_corpus.iloc[index]['name'],
-                'score':  int(cosine_similarities[index]*100)
-            }
-            for index in related_product_indices
-        ]
+#         cosine_similarities = cosine_similarity(
+#             vectorized_text,  self.tf_idf_data).flatten()
+#         related_product_indices = cosine_similarities.argsort()[:-number:-1]
+#         return [
+#             {
+#                 'citate': self.citate_corpus.iloc[index]['line'],
+#                 'author': self.citate_corpus.iloc[index]['author'],
+#                 'title': self.citate_corpus.iloc[index]['name'],
+#                 'score':  int(cosine_similarities[index]*100)
+#             }
+#             for index in related_product_indices
+#         ]
 
 
-convert_instance = QueryConverter()
+# convert_instance = QueryConverter()
 
 
 @api_view(('GET',))
@@ -72,7 +72,8 @@ def verify(request):
 
     query = input_data.validated_data['query']
     citate_number = input_data.validated_data['number']
-    similar_lines = convert_instance.find_similar(query, citate_number)
+    similar_lines = []
+    # convert_instance.find_similar(query, citate_number)
 
     best_score = max(item['score'] for item in similar_lines) or 0
     output_data = VerifyOutputSerializer(data={
