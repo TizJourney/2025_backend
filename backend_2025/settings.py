e94c110bf964caf58990205d6c75072da5822b7b
Konstantin Bondar <kostya.bondar@gmail.com>
2020-12-20 18:20:13 +0300

    feat: поправить проверки CORPS

--- backend_2025/settings.py
+++ backend_2025/settings.py
@@ -33,9 +33,9 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',    
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -114,7 +114,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 CORS_ORIGIN_ALLOW_ALL = True
-CORS_URLS_REGEX = r'^/api/.*$'
 
 # Activate Django-Heroku.
 django_heroku.settings(locals())
